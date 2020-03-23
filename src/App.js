c6a405d00e6e68d0fe50b9d5b9377b4b5449224d
waog <oliver.stadie@gmail.com>
2020-03-23 15:48:54 +0100

    CLEAN UP todo list component with seperated data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from "react";
 import "./App.css";
-import logo from "./logo.svg";
+
+const todos = ["Homework", "Clean Up", "Wash dishes"];
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+function TodoListFn(props) {
+  return (
+    <ul className="todo-list">
+      {/* <TodoElementsFn todos={todos} /> */}
+      {props.todos.map((todo, index) => (
+        <TodoElementFn key={index} todo={todo} />
+      ))}
+    </ul>
+  );
+}
+
+function TodoElementFn(props) {
+  return <li className="todo-item">{props.todo}</li>;
+}
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello react-kata!</p>
+      <Welcome name="react-kata!"></Welcome>
+      <h1>ToDos:</h1>
+      <TodoListFn todos={todos} />
     </div>
   );
 }
+
+setInterval(() => {
+  todos.push("more");
+  console.log(`pushed`);
+}, 1000);
 
 export default App;
